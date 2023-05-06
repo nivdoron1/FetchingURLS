8ea70b0de1fd40a36f4bf6bc2caa5b4aec01a698
Niv Doron <niv.doron@post.runi.ac.il>
2023-05-07 02:20:55 +0300

    third commit- make an input from the user option.

--- src/main/java/ExecuteHtml.java
+++ src/main/java/ExecuteHtml.java
@@ -1,8 +1,22 @@
 import java.io.IOException;
+import java.util.Scanner;
 
 public class ExecuteHtml {
     public static void main(String[] args) throws IOException {
-        Source source = new Source("https://www.n12.co.il/",5,3,true);
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter your URL website:");
+        String url = scanner.nextLine();
+
+        System.out.println("Enter your max amount of URL's that you want to execute each level:");
+        int maxAmount = scanner.nextInt();
+
+        System.out.println("Enter the depth factor:");
+        int depthFactor = scanner.nextInt();
+
+        System.out.println("Enter uniqueness write (true/false):");
+        boolean crossLevelUniqness = scanner.nextBoolean();
+
+        Source source = new Source(url, maxAmount, depthFactor, crossLevelUniqness);
         UrlAdder urlAdder = new UrlAdder(source);
         urlAdder.run();
     }
